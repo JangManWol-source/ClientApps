3c7fd6c6838ceb537cd5d49685345cf7b9eccaac
Joe Cristian Jamis <85830010+JangManWol-source@users.noreply.github.com>
2022-07-12 17:32:23 +0800

    Add project files.

---
+++ ClientApps/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ClientApps
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
